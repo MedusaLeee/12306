d768bd00b3d08b953759f330bc2571e2316efc3d
jianxun <medusa.leee@gmail.com>
2018-02-21 20:26:46 +0800

    封装axios的post方法

--- src/utils.js
+++ src/utils.js
@@ -30,6 +30,29 @@ const httpGet = async(url, option) => {
 };
 
 /**
+ * 使用cookieJar封装axios的post请求
+ * @param url
+ * @param body
+ * @param option
+ * @returns {Promise<AxiosPromise<any>>}
+ */
+const httpPost = async(url, body, option) => {
+    const ua = rua.getRandom();
+    const headers = {
+        'User-Agent': ua,
+        'Host': 'kyfw.12306.cn',
+        ...option.headers
+    };
+    option.headers = headers;
+    const newOption = {
+        jar: cookieJar,
+        withCredentials: true,
+        ...option
+    };
+    return axios.post(url, body, newOption);
+};
+
+/**
  * 根据验证码编号获取验证码校验请求所需的坐标字符串
  * @param numberString
  * @returns {string}
@@ -49,6 +72,7 @@ const getLogger = (category) => {
 module.exports = {
     getCaptchaString,
     httpGet,
+    httpPost,
     getLogger
 };
 
